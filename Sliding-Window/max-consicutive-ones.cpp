8283bdf3f8be4224907e37774e46e9d3e3bf8ab2
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-08-03 12:41:22 +0530

    method 2

--- Sliding-Window/max-consicutive-ones.cpp
+++ Sliding-Window/max-consicutive-ones.cpp
@@ -27,3 +27,26 @@ int longestOnes(vector<int>& nums, int k) {
     }
     return maxlen;
 }
+
+
+// Method-2
+int longestOnes(vector<int>& nums, int k) {
+    int n = nums.size();
+    int l = 0, r = 0;
+    int maxlen = 0;
+    int zero = 0;
+    while (r < n) {
+        if (nums[r] == 0) {
+            zero++;
+        }
+        if(zero > k) {
+            if (nums[l] == 0)
+                zero--;
+            l++;
+        } else{
+            maxlen = max(maxlen, r - l + 1);
+        }
+        r++;
+    }
+    return maxlen;
+}
