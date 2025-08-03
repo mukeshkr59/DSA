ada9fd845707105a57ae56cf53aad54cc6b6a401
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-08-03 12:29:43 +0530

    initial commit

---
+++ Sliding-Window/max-consicutive-ones.cpp
@@ -0,0 +1,29 @@
+// 1004. Max Consecutive Ones III
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Method-1
+
+int longestOnes(vector<int>& nums, int k) {
+    int n = nums.size();
+    int l = 0, r = 0;
+    int maxlen = 0;
+    int zero = 0;
+    while (r < n) {
+        if (nums[r] == 0) {
+            zero++;
+        }
+        while(zero > k) {
+            if (nums[l] == 0)
+                zero--;
+            l++;
+        }
+        if (zero <= k) {
+            maxlen = max(maxlen, r - l + 1);
+        }
+        r++;
+    }
+    return maxlen;
+}
