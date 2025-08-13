e997410a2b20713915645945dbabaa33e4a4e3e5
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-08-13 21:29:36 +0530

    BT- Height

--- Binary Tree/Traversal-Binary-Tree.cpp
+++ Binary Tree/Traversal-Binary-Tree.cpp
@@ -30,6 +30,31 @@ vector<int> preorderTraversal(TreeNode *root)
     return ans;
 }
 
+// Irerative PreOrder Traversal
+vector<int> preorderTraversal(TreeNode *root)
+{
+    vector<int> preorder;
+    if (root == NULL)
+        return preorder;
+    stack<TreeNode *> st;
+    st.push(root);
+    while (!st.empty())
+    {
+        root = st.top();
+        st.pop();
+        preorder.push_back(root->val);
+        if (root->right != NULL)
+        {
+            st.push(root->right);
+        }
+        if (root->left != NULL)
+        {
+            st.push(root->left);
+        }
+    }
+    return preorder;
+}
+
 // postorder Traversal
 vector<int> postorderTraversal(TreeNode *root)
 {
